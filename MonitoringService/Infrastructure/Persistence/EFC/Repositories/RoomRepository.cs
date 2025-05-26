ef04c5c0259f0eb061bac55ea6a699f3566f4409
aaron acuña <aaronacu_85@hotmail.com>
2025-05-26 08:56:46 -0500

    feat: added infrastructure.

---
+++ MonitoringService/Infrastructure/Persistence/EFC/Repositories/RoomRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonitoringService.Domain.Model.Aggregates;
+using MonitoringService.Domain.Model.ValueObjects.Room;
+using MonitoringService.Domain.Repositories;
+using MonitoringService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using MonitoringService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace MonitoringService.Infrastructure.Persistence.EFC.Repositories
+{
+    public class RoomRepository
+        (MonitoringContext context) :
+        BaseRepository<Room>(context),
+        IRoomRepository
+    {
+        public async Task<bool> UpdateRoomStateAsync
+            (int id, ERoomState roomState) =>
+            await Context.Set<Room>().Where(r => r.Id == id)
+            .ExecuteUpdateAsync(r => r
+            .SetProperty(u => u.State, roomState.ToString())) > 0;
+
+        public async Task<IEnumerable<Room>> FindByTypeRoomIdAsync
+            (int typeRoomId) => await Context.Set<Room>()
+            .Where(r => r.TypesRoomsId == typeRoomId)
+            .ToListAsync();
+
+        public async Task<IEnumerable<Room>> FindAllByHotelId(int hotelId)
+        {
+            Task<IEnumerable<Room>> queryAsync = new(() => (
+                from rm in Context.Set<Room>().ToList()
+                where rm.HotelsId.Equals(hotelId)
+                select rm
+            ).ToList());
+
+            queryAsync.Start();
+            
+            var result = await queryAsync;
+
+            return result;
+        }
+    }
+}
---
+++ MonitoringService/Infrastructure/Persistence/EFC/Repositories/TypeRoomRepository.cs
@@ -0,0 +1,30 @@
+﻿using MonitoringService.Domain.Model.Aggregates;
+using MonitoringService.Domain.Model.Entities;
+using MonitoringService.Domain.Repositories;
+using MonitoringService.Shared.Infrastructure.Persistence.EFC.Configuration;
+using MonitoringService.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace MonitoringService.Infrastructure.Persistence.EFC.Repositories
+{
+    public class TypeRoomRepository
+        (MonitoringContext context) :
+        BaseRepository<TypeRoom>(context),
+        ITypeRoomRepository
+    {
+        public async Task<IEnumerable<TypeRoom>> FindAllByHotelId(int hotelId)
+        {
+            Task<IEnumerable<TypeRoom>> queryAsync = new (() => (
+                from tr in Context.Set<TypeRoom>().ToList()
+                join ro in Context.Set<Room>().ToList() on tr.Id equals ro.TypesRoomsId
+                where ro.HotelsId.Equals(hotelId)
+                select tr
+            ).ToList());
+
+            queryAsync.Start();
+
+            var result = await queryAsync;
+
+            return result;
+        }
+    }
+}
